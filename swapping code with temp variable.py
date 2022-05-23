2089df703968b190c4950a66b6d80c05e7d7c3fb
Mayuresh5693 <103833531+Mayuresh5693@users.noreply.github.com>
2022-05-23 13:02:46 +0530

    swapping code with temp variable

---
+++ swapping code with temp variable.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 20:19:58 2022
+
+@author: mayureshdhule
+"""
+
+#code for swapping two interger with temp variable
+a = int(input('Please Enter 1st number:a = '))
+b = int(input('Please Enter 2nd number:b = '))
+# put 'a' value in another temp variable i.e 'x'
+x=a
+a=b
+b=x
+
+print ('after swapping')
+
+print ('a =',a)
+print ('b =',b)
+
